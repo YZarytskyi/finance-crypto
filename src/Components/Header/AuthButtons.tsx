6dd9bf3e75d0e1ec71f4574402058c1de9e76eec
Yurii Zarytskyi <yurazar222@gmail.com>
2022-11-26 00:06:16 +0200

    refactor mobile auth

--- src/Components/Header/AuthButtons.tsx
+++ src/Components/Header/AuthButtons.tsx
@@ -1,31 +1,30 @@
 import style from "./Header.module.scss";
 import { useRef, useState } from "react";
 import Auth from "../Auth/Auth";
-import { COOKIE_TOKEN_NAME, deleteCookie, getCookie } from "../../utils/cookie";
+import { COOKIE_TOKEN_NAME, deleteCookie } from "../../utils/cookie";
 
 interface AuthButtonsProps {
   setIsProfileOpen?: (
     value: boolean | ((prevValue: boolean) => boolean)
   ) => void;
+  isProfileOpen?: boolean;
+  userId?: string | undefined;
 }
 
-const AuthButtons: React.FC<AuthButtonsProps> = ({ setIsProfileOpen }) => {
+const AuthButtons: React.FC<AuthButtonsProps> = ({ setIsProfileOpen, isProfileOpen, userId }) => {
   const [modalAuthShow, setModalAuthShow] = useState<boolean>(false);
-  const [toggleLoginSignUp, setToggleLoginSignUp] = useState<boolean>(true);
+  const [isLogin, setIsLogin] = useState<boolean>(true);
 
   const loginRef = useRef(null);
-
-  const userId: string | null = getCookie(COOKIE_TOKEN_NAME);
 
   const onClickModalAuth = (e: React.SyntheticEvent) => {
     e.preventDefault();
     const target = e.target as Element;
     setModalAuthShow(true);
-    // setIsProfileOpen && setIsProfileOpen(false);
     if (target === loginRef.current) {
-      setToggleLoginSignUp(true);
+      setIsLogin(true);
     } else {
-      setToggleLoginSignUp(false);
+      setIsLogin(false);
     }
   };
 
@@ -36,42 +35,42 @@ const AuthButtons: React.FC<AuthButtonsProps> = ({ setIsProfileOpen }) => {
   };
 
   return (
-    <div className={style.authContainer}>
-      {userId ? (
-        <button
-          type="button"
-          className={style.logoutBtn}
-          onClick={(e) => onClickLogout(e)}
-        >
-          Log&nbsp;Out
-        </button>
-      ) : (
-        <>
-          <button
-            ref={loginRef}
-            type="button"
-            className={style.loginBtn}
-            onClick={(e) => onClickModalAuth(e)}
-          >
-            Login
-          </button>
+    <>
+      <div className={`${style.authContainerHidden} ${isProfileOpen ? style.authContainerOpen : ""}`}>
+        {userId ? (
           <button
             type="button"
-            className={style.signUpBtn}
-            onClick={(e) => onClickModalAuth(e)}
+            className={style.logoutBtn}
+            onClick={(e) => onClickLogout(e)}
           >
-            Sign&nbsp;Up
+            Log&nbsp;Out
           </button>
-
-          <Auth
-            modalAuthShow={modalAuthShow}
-            setModalAuthShow={setModalAuthShow}
-            toggleLoginSignUp={toggleLoginSignUp}
-            setToggleLoginSignUp={setToggleLoginSignUp}
-          />
-        </>
-      )}
-    </div>
+        ) : (
+          <>
+            <button
+              ref={loginRef}
+              type="button"
+              className={style.loginBtn}
+              onClick={(e) => onClickModalAuth(e)}
+            >
+              Login
+            </button>
+            <button
+              type="button"
+              className={style.signUpBtn}
+              onClick={(e) => onClickModalAuth(e)}
+            >
+              Sign&nbsp;Up
+            </button>
+          </>
+        )}
+      </div>
+      <Auth
+        modalAuthShow={modalAuthShow}
+        setModalAuthShow={setModalAuthShow}
+        isLogin={isLogin}
+      />
+    </>
   );
 };
 
--- src/Components/Header/MobileNav.tsx
+++ src/Components/Header/MobileNav.tsx
@@ -4,10 +4,25 @@ import sprite from "../../assets/images/icons.svg";
 import { NavLink } from "react-router-dom";
 import React, { useState } from "react";
 import AuthButtons from "./AuthButtons";
+import { COOKIE_TOKEN_NAME, getCookie } from "../../utils/cookie";
+import Auth from "../Auth/Auth";
 
 const MobileNav = () => {
   const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
   const [isProfileOpen, setIsProfileOpen] = useState<boolean>(false);
+
+  const userId: string | null = getCookie(COOKIE_TOKEN_NAME);
+
+  const hamburgerIcon = (
+    <svg className={style.hamburgerIcon}>
+      <use href={sprite + "#menu_open"} />
+    </svg>
+  );
+  const closeIcon = (
+    <svg className={style.closeIcon}>
+      <use href={sprite + "#menu_close"} />
+    </svg>
+  );
 
   const handleToggleMenu = () => {
     openMenu(isMenuOpen, setIsMenuOpen);
@@ -26,17 +41,6 @@ const MobileNav = () => {
       document.body.style.overflowY = "hidden";
     } else document.body.style.overflowY = "scroll";
   }
-
-  const hamburgerIcon = (
-    <svg className={style.hamburgerIcon}>
-      <use href={sprite + "#menu_open"} />
-    </svg>
-  );
-  const closeIcon = (
-    <svg className={style.closeIcon}>
-      <use href={sprite + "#menu_close"} />
-    </svg>
-  );
 
   const onClickMobileLink = (): void => {
     setIsMenuOpen(false);
@@ -79,14 +83,22 @@ const MobileNav = () => {
             <use href={sprite + "#profile"} />
           </svg>
         </button>
-        {isProfileOpen && (
-          <div
-            className={style.authBtnsContainer}
-            onClick={(e) => onClickCloseProfile(e)}
-          >
-            <AuthButtons setIsProfileOpen={setIsProfileOpen}/>
-          </div>
-        )}
+        {userId
+          ? (
+            <div
+              className={`${style.authBtnsBackdropHidden} ${isProfileOpen ? style.authBtnsBackdropOpen : ""}`}
+              onClick={(e) => onClickCloseProfile(e)}
+            >
+              <AuthButtons setIsProfileOpen={setIsProfileOpen} isProfileOpen={isProfileOpen} userId={userId} />
+            </div>
+          )
+          : (
+            <Auth
+              modalAuthShow={isProfileOpen}
+              setModalAuthShow={setIsProfileOpen}
+              isLogin
+            />
+          )}
       </div>
     </div>
   );
