5b0e0c1f23c921fe89f379ecf0bcebafb893871c
YZarytskyi <yurazar222@gmail.com>
2022-12-15 10:01:04 +0200

    refactor contact page

--- src/Components/Contacts/ContactsForm.tsx
+++ src/Components/Contacts/ContactsForm.tsx
@@ -4,7 +4,7 @@ import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import SendButton from "@mui/material/Button";
-import Modal from "../Common/Modal"
+import Modal from "../Common/Modal";
 
 const schema = yup.object({
   fullName: yup.string().required("⚠ This field is required"),
@@ -31,32 +31,54 @@ const ContactsForm = () => {
   const onSubmit = (data: IFormInputs) => setModalShow(true);
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className={style.contactsForm}>
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className={style.contactsForm}
+      autoComplete="off"
+    >
       <label className={style.formLabel}>
         Full Name
-        <input {...register("fullName")} className={style.formInput} />
-        <p className={style.formErrorMessage}>{errors.fullName?.message ? errors.fullName?.message : " "}</p>
+        <input
+          {...register("fullName")}
+          className={style.formInput}
+          placeholder="Enter your name..."
+        />
+        <p className={style.formErrorMessage}>
+          {errors.fullName?.message ? errors.fullName?.message : " "}
+        </p>
       </label>
       <label className={style.formLabel}>
         Email
-        <input {...register("email")} className={style.formInput} />
-        <p className={style.formErrorMessage}>{errors.email?.message ? errors.email?.message : " "}</p>
+        <input
+          {...register("email")}
+          className={style.formInput}
+          placeholder="Enter your email..."
+        />
+        <p className={style.formErrorMessage}>
+          {errors.email?.message ? errors.email?.message : " "}
+        </p>
       </label>
       <label className={style.formLabel}>
         Message
-        <textarea {...register("message")} className={style.formTextarea} />
-        <p className={style.formErrorMessage}>{errors.message?.message ? errors.message.message : " "}</p>
+        <textarea
+          {...register("message")}
+          className={style.formTextarea}
+          placeholder="Enter your message..."
+        />
+        <p className={style.formErrorMessage}>
+          {errors.message?.message ? errors.message.message : " "}
+        </p>
       </label>
       <SendButton type="submit" variant="outlined" className={style.formButton}>
         Send
       </SendButton>
-    <Modal modalShow={modalShow} setModalShow={setModalShow}>
-      <h1 className={style.modalTitle}>Thank you for getting in touch!</h1>
-      <p className={style.modalText}>
-        We have received your message and would like to thank you for writing
-        to us. We will reply by email as soon as possible.
-      </p>
-    </Modal>
+      <Modal modalShow={modalShow} setModalShow={setModalShow}>
+        <h1 className={style.modalTitle}>Thank you for getting in touch!</h1>
+        <p className={style.modalText}>
+          We have received your message and would like to thank you for writing
+          to us. We will reply by email as soon as possible.
+        </p>
+      </Modal>
     </form>
   );
 };
